cb61bd103608a62a2624e420f49a65d52e6b963a
HenryPineda <pinedahenryandres@gmail.com>
2019-12-01 16:16:26 -0600

    Clean up of the App.js file after part 86

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import classes from './App.module.css';
-// import Radium, { StyleRoot } from 'radium';
+
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-// import UserInput from '../components/UserInput/UserInput';
-// import UserOutput from '../components/UserOutput/UserOutput';
 import Validation from '../components/Validation/Validation';
 import Char from '../components/Char/Char';
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
@@ -87,17 +85,6 @@ class App extends Component {
 
   }
 
-  // changeInputHangler =(event) => {
-
-  //   this.setState({
-
-  //     users: [
-
-  //       {username: event.target.value, alias:this.state.users[0].alias}
-
-  //     ]
-  //   })
-  // }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -147,53 +134,14 @@ class App extends Component {
       return <Char char={char} click={() =>this.deleteCharHandler(index)} key={index} />
     });
 
-    const style = {
-
-      backgroundColor: 'green',
-      color:'white',
-      font:'inherti',
-      border:'1px solid blue',
-      padding:'8px',
-      borderRadius:'5px',
-      cursor:'pointer',
-      ':hover':{
-
-        backgroundColor:'lightgreen',
-        color:'black'
-      }
-    }
 
     let persons = null;
-
-    
 
     if(this.state.showPersons){
 
       persons = (
 
         <div>
-
-            {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler} >
-              My Hobby is: Racing
-            </Person>
-
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.changedNameHandler} />
-
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Henry Torrez')}>
-              My Hobbie is: Play with paw patrol
-            </Person>
-
-            <UserInput changeInput={this.changeInputHangler} name={this.state.users[0].username} />
-
-            <button onClick={this.changeUser.bind(this, 'Ricardo', 'Perro')}>Change user</button>
-            <UserOutput userName={this.state.users[0].username} userAlias ={this.state.users[0].alias} /> */}
-
-            {/* {this.state.persons.map((person, index) => {
-
-                // <ErrorBoundary></ErrorBoundary>
-                return <Person key={person.id} name={person.name} age = {person.age} click={() => this.deletePersonHandler(index)}  changed={(event) =>this.changedNameHandler(event, person.id)} >This is a children of person!</Person>
-                      
-            })} */}
 
             <Persons 
               persons={this.state.persons}
@@ -205,56 +153,12 @@ class App extends Component {
         </div>
         
         )
-      // style.backgroundColor ='red';
-      // style[':hover'] ={
 
-      //   backgroundColor:'salmon',
-      //   color:'black'
-      // }
-
-      // btnClass = classes.Red;
     }
-
-    // let classes2 = [];
-
-    // if(this.state.persons.length <= 2){
-
-    //   classes2.push(classes.red);
-    // }
-
-    // if(this.state.persons.length <=1){
-
-    //   classes2.push(classes.bold);
-    // }
 
     return (
 
-    // <StyleRoot >
-
       <div className={classes.App}>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-
-        {/* <p className={classes2.join(' ')}>Hi, I am a react app!</p>
-        <button
-          //style = {style}
-          // onClick= {this.switchNameHandler.bind(this, 'Henry Pineda')}>Switch name
-          className={btnClass}
-          onClick = {this.togglePersonsHandler}
-        >Show people
-        </button> */}
 
         <Cockpit
           persons={this.state.persons}
@@ -263,31 +167,6 @@ class App extends Component {
         >
             
         </Cockpit>
-          
-        {/* {
-
-          this.state.showPersons ===true ?
-          <div>
-
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler} >
-              My Hobby is: Racing
-            </Person>
-
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.changedNameHandler} />
-
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Henry Torrez')}>
-              My Hobbie is: Play with paw patrol
-            </Person>
-
-            <UserInput changeInput={this.changeInputHangler} name={this.state.users[0].username} />
-
-            <button onClick={this.changeUser.bind(this, 'Ricardo', 'Perro')}>Change user</button>
-            <UserOutput userName={this.state.users[0].username} userAlias ={this.state.users[0].alias} />
-          </div>
-
-          : null
-
-        } */}
 
         {persons}
 
@@ -301,9 +180,8 @@ class App extends Component {
         <Validation inputLength={this.state.userInput.length}  />
 
         {charList}
-      </div>
 
-    // </StyleRoot>
+      </div>
 
     )
 
